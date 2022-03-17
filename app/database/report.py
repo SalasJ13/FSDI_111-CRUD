2756c3afd1ec14e2609f2e6d0943924804ae9af8
Salas <94945634+SalasJ13@users.noreply.github.com>
2022-03-17 14:58:31 -0700

    Update report.py

--- app/database/report.py
+++ app/database/report.py
@@ -2,21 +2,23 @@ from app.database import get_db
 
 
 def get_users_and_vehicles_join():
-    stm = """
+    stmt = """
         select 
         user.last_name,
         user.first_name,
         user.hobbies,
         user.active, 
-        vehicle.license_plate,
         vehicle.color,
-        vehicle.v_type as vehicle_type,
-        vehicle_type.description 
-        from user inner join vehicle on user.id=vehicle.user_id 
-        inner join vehicle_type on vehicle.v_type = vehicle_type;"
+        vehicle.license_plate,
+        vehicle_type.description  
+        from user 
+        inner join vehicle on user.id = vehicle.user_id 
+        inner join vehicle_type on vehicle.v_type = vehicle_type.id
+        
     """
-    cursor = get_db().execute(stm, ())
+    cursor = get_db().execute(stmt, ())
     results = cursor.fetchall()
+    cursor.close()
     out = []
     for result in results:
         res_dict = {}
@@ -24,10 +26,10 @@ def get_users_and_vehicles_join():
             "user_first_name": result[0],
             "user_last_name": result[1],
             "user_hobbies": result[2],
-            "user_activate": "true" if result[3] == 1 else "false",
-            "vehicle_license_plate": result[4],
-            "vehicle_color": result[5],
-            "vehicle_type": result[6]
+            "user_activate": True if result[3] == 1 else False,
+            # "vehicle_license_plate": result[4],
+            "vehicle_color": result[4],
+            "vehicle_type": result[5]
         }
         out.append(res_dict)
     return out
